0a0093a3ddba4783692cb1afa0ca817108a8e3cf
Khramenko Vladislav Vladimirovich <vladhromenko@gmail.com>
2025-05-26 08:09:49 +0300

    fixes in front adaptive

--- frontend/src/pages/ProductPage.tsx
+++ frontend/src/pages/ProductPage.tsx
@@ -317,7 +317,7 @@ const ProductPage: React.FC = () => {
                             padding: screens.xs ? '4px 8px' : '6px 15px',
                           }}
                         >
-                          {!screens.xs && "В корзину"}
+                          "В корзину"
                         </Button>
                     ]}
                   >
